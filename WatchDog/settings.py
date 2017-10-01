0288fd4d1da4d243997594dd1d9d309689934546
Yan Cao <cyandterry@hotmail.com>
2017-10-01 16:34:30 -0700

    Update piplines

--- WatchDog/settings.py
+++ WatchDog/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for Supreme project
+# Scrapy settings for WatchDog project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'WatchDog.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'Supreme (+http://www.yourdomain.com)'
+#USER_AGENT = 'WatchDog (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -47,13 +47,13 @@ ROBOTSTXT_OBEY = True
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'Supreme.middlewares.SupremeSpiderMiddleware': 543,
+#    'WatchDog.middlewares.WatchdogSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'Supreme.middlewares.MyCustomDownloaderMiddleware': 543,
+#    'WatchDog.middlewares.MyCustomDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-   'pipelines.CSVPipeline': 300,
-}
+#ITEM_PIPELINES = {
+#    'WatchDog.pipelines.WatchdogPipeline': 300,
+#}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
