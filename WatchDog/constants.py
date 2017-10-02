5a00266059804df62c5cf41235bfc6d6fb8798b4
Yan Cao <cyandterry@hotmail.com>
2017-10-01 17:30:27 -0700

    Adding constants

---
+++ WatchDog/constants.py
@@ -0,0 +1,23 @@
+BASE_URL = 'http://www.supremenewyork.com'
+
+
+class ProductType:
+    BAG = 'bags'
+    TOP_SWEATERS = 'tops-sweaters'
+    SHOE = 'shoes'
+    SWEATSHIRT = 'sweatshirts'
+    HAT = 'hats'
+    ACCESSORY = 'accessories'
+    SKATE = 'skate'
+    SHIRT = 'shirts'
+    JACKET = 'jackets'
+    T_SHIRT = 't-shirts'
+    PANTS = 'pants'
+
+
+TOP_PRIORITY_PRODUCTS = [
+    ProductType.BAG,
+    ProductType.SWEATSHIRT,
+    ProductType.T_SHIRT,
+    ProductType.TOP_SWEATERS,
+]
